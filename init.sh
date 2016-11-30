b6d691cbea743e493c1199df300daee14eb965a2
hissDev <rafael.hiss@gmail.com>
2016-11-30 16:48:51 +0100

    wip

---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+drush dl -y features
+drush en -y features
+drush dl -y diff
+drush en -y diff
+drush en -y fu_hilfetexte
+drush en -y fu_ansicht
+
+drush cc all
+
+drush fr -y fu_hilfetexte
+drush fr -y fu_ansicht
