008eae2c55c9830ca715daa29d48adff222d6e9b
hissDev <rafael.hiss@gmail.com>
2016-12-29 10:16:11 +0100

    Dev (#2)
    * test commit
    
    * debv
    
    * WIP: ToDos Bereich "Eigene Bilder einstellen"
    
    * Update test.txt
    
    * initial static commit
    
    * wip
    
    * dann schaun mer mal
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * check feature settings
    
    * feature FIX
    
    * WIP feature export TODO check strongarm for fu_ansicht
    
    * wip
    
    * template integration
    
    * template integration
    
    * template integration
    
    * template integration
    
    * template integration
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * term count
    
    * term count sort
    
    * deleted unneeded install profile test
    
    * Fixes - autocomplete - hide fields
    
    * Style update
    
    * wip
    
    * wip
    
    * wip

--- init.sh
+++ init.sh
@@ -1,37 +1,49 @@
 #!/usr/bin/env bash
 
+
 #git pull origin dev
+mkdir -p ../db_backup
+drush sql-dump > ../db_backup/$(date +"%Y-%m-%d__%H-%M-%S".sql)
+
+## delete old unsused module references - module has been deleted somewhen
+drush sql-query "DELETE from system where name = 'mass_contact' AND type = 'module';"
+drush sql-query "DELETE from system where name = 'user_restrictions_ui' AND type = 'module';"
+drush sql-query "DELETE from system where name = 'user_restrictions' AND type = 'module';"
+
 
 ## delete menu link 626 :)
-#drush ev "menu_link_delete(626);"
-#drush ev "module_load_include('inc', 'locale', 'menu');menu_link_delete(626);"
-#
-#drush dl -y strongarm
-#drush en -y strongarm
-#drush dl -y features
-#drush en -y features
-#drush dl -y diff
-#drush en -y diff
+drush ev "menu_link_delete(626);"
+drush ev "module_load_include('inc', 'locale', 'menu');menu_link_delete(626);"
+
+
+echo "##################  menu item"
+
+drush dl -y strongarm
+drush en -y strongarm
+echo "##################  strongarm"
+drush dl -y features
+drush en -y features
+echo "##################  features"
+drush dl -y diff
+drush en -y diff
+echo "##################  diff"
 drush dl -y autocomplete_deluxe
-#drush en -y autocomplete_deluxe
-#drush dl field_placeholder 7.x-1.0
-#drush en -y field_placeholder
-#drush en -y fu_hilfetexte
-#drush en -y fu_ansicht
-#
-#
-#drush cc all
-#
-#drush fr -y fu_hilfetexte
-#drush fr -y fu_ansicht
-#
+drush en -y autocomplete_deluxe
+echo "##################  autocomplete_deluxe"
+drush dl -y field_placeholder 7.x-2.0-beta2
+drush en -y field_placeholder
+echo "##################  field_placeholder"
+drush en -y fu_hilfetexte
+drush en -y fu_ansicht
+echo "##################  fu_hilfetexte && fu_ansicht"
+
+drush cc all
+
+drush fr -y fu_hilfetexte
+drush fr -y fu_ansicht
+
 #drush user-password rafael --password="rafael_fuhi_2016"
-#drush user-create test --mail="test@test.com" --password="test"
-#
-
-
-cp patches/autocomplete_deluxe-count-sort.patch ../autocomplete_deluxe/autocomplete_deluxe-count-sort.patch
-cd .. && cd autocomplete_deluxe && patch < autocomplete_deluxe-count-sort.patch
-
+drush user-create rafael --mail="rafael.hiss@gmail.com" --password="rafael_fuhi_2016"
+drush user-create test --mail="test@test.com" --password="test"
 
 drush cc all
