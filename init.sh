1bd25af9b811699688503e98ed5e806a3b6a0408
hissDev <rafael.hiss@gmail.com>
2016-12-29 10:11:19 +0100

    wip

--- init.sh
+++ init.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
+
+
 #git pull origin dev
 mkdir -p ../db_backup
 drush sql-dump > ../db_backup/$(date +"%Y-%m-%d__%H-%M-%S".sql)
 
+## delete old unsused module references - module has been deleted somewhen
+drush sql-query "DELETE from system where name = 'mass_contact' AND type = 'module';"
+drush sql-query "DELETE from system where name = 'user_restrictions_ui' AND type = 'module';"
+drush sql-query "DELETE from system where name = 'user_restrictions' AND type = 'module';"
+
+
 ## delete menu link 626 :)
 drush ev "menu_link_delete(626);"
 drush ev "module_load_include('inc', 'locale', 'menu');menu_link_delete(626);"
+
 
 echo "##################  menu item"
 
@@ -22,7 +31,7 @@ echo "##################  diff"
 drush dl -y autocomplete_deluxe
 drush en -y autocomplete_deluxe
 echo "##################  autocomplete_deluxe"
-drush dl -y field_placeholder 7.x-1.0
+drush dl -y field_placeholder 7.x-2.0-beta2
 drush en -y field_placeholder
 echo "##################  field_placeholder"
 drush en -y fu_hilfetexte
