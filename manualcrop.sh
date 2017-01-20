ec2e4a993ced18c212549ce6e1b564ad531d80b4
rafael <rafael.hiss@freieformation.de>
2017-01-20 17:05:08 +0100

    crop patch

--- manualcrop.sh
+++ manualcrop.sh
@@ -4,18 +4,19 @@
 #git pull origin dev
 mkdir -p ../../../../../db_backup
 drush sql-dump > ../../../../../db_backup/$(date +"%Y-%m-%d__%H-%M-%S".sql)
+#
+#cp -avr patches/files/jquery.imagesloaded ../../libraries
+#cp -avr patches/files/jquery.imgareaselect ../../libraries
+#
+#drush cc all
+#drush dl -y manualcrop
+#drush en -y manualcrop
 
-cp -avr patches/files/jquery.imagesloaded ../../libraries
-cp -avr patches/files/jquery.imgareaselect ../../libraries
-
-drush cc all
-drush dl -y manualcrop
-drush en -y manualcrop
+cp patches/manualcrop.js ../manualcrop/manualcrop.js
 
 
-
-cp patches/manualcrop_modify.patch ../manualcrop/manualcrop_modify.patch
-cd .. && cd manualcrop && patch < manualcrop_modify.patch
+#cp patches/manualcrop_modify.patch ../manualcrop/manualcrop_modify.patch
+#cd .. && cd manualcrop && patch < manualcrop_modify.patch
 echo "##################  PATCH"
 
 #drush fr -y fu_ansicht
