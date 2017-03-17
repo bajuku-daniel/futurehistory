29156995d5cc31339a0ec4f18801de1c9b353872
rafael <rafael.hiss@freieformation.de>
2017-03-17 12:43:00 +0100

    update helper

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+git pull origin dev
+
+#mkdir -p ../../../../../db_backup
+#drush sql-dump > ../../../../../db_backup/$(date +"%Y-%m-%d__%H-%M-%S".sql)
+#drush sql-dump > ../db_backup/$(date +"%Y-%m-%d__%H-%M-%S".sql)
+#drush sql-drop -y
+#drush sql-cli < 2017-02-01__15-05-29.sql
+
+
+cd ../../themes/future_history/ && git pull origin dev && cd -
+
+drush cc all
