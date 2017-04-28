9321de6e78eb82ab8dd8a6d4b8b752b0711f5db3
hissDev <rafael.hiss@gmail.com>
2017-04-28 17:36:23 +0200

    wip

--- update.sh
+++ update.sh
@@ -7,10 +7,10 @@ drush sql-dump > ../../../../../db_backup/$(date +"%Y-%m-%d__%H-%M-%S".sql)
 drush sql-drop -y
 drush sql-cli < ../../../../../db_backup/2017-04-28__16-15-01_from_live.sql
 
-drush fr -y map_view_update
-
 cd ../../themes/future_history/ && git pull origin dev && cd -
 cd ../futurehistory_entdecken/ && git pull origin dev_rh && cd -
+
+drush fr -y map_view_update
 
 drush user-password Romalu3 --password="test"
 
