2a9fd8dbaa4456060d90cfbf407d2eb3eb68895e
hissDev <rafael.hiss@gmail.com>
2017-05-03 17:06:20 +0200

    update script

--- update.sh
+++ update.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-git pull origin dev
-
 mkdir -p ../../../../../db_backup
 drush sql-dump > ../../../../../db_backup/$(date +"%Y-%m-%d__%H-%M-%S".sql)
 drush sql-drop -y
 drush sql-cli < ../../../../../db_backup/2017-04-28__16-15-01_from_live.sql
 
-
+git pull origin dev
 cd ../futurehistory_entdecken/ && git pull origin dev_rh && cd -
 cd ../../themes/future_history/ && git pull origin dev && cd -
+
+
 drush cc all
 drush dis -y overlay
 drush fr -y map_view_update
