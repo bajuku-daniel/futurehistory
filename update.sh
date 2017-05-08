cc4652668dd32cdd58ee23b5a931b59df6501a00
hissDev <rafael.hiss@gmail.com>
2017-05-08 17:39:51 +0200

    wip

--- update.sh
+++ update.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+#mkdir -p ../../../../../db_backup
+#drush sql-dump > ../../../../../db_backup/$(date +"%Y-%m-%d__%H-%M-%S".sql)
+#drush sql-drop -y
+#drush sql-cli < ../../../../../db_backup/2017-04-28__16-15-01_from_live.sql
+
+
+
 git pull origin master
 cd ../../themes/future_history/ && git pull origin master && cd -
 cd ../futurehistory/ && git pull origin master && cd -
@@ -10,5 +17,7 @@ drush cc all
 drush dis -y overlay
 drush en -y map_view_update
 drush fr -y map_view_update
+
+drush user-password Romalu3 --password="test"
 
 drush cc all
