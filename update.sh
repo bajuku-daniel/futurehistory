5dd531c4af9670f3384bc656a8143b81d2a171d4
hissDev <rafael.hiss@gmail.com>
2017-05-08 17:53:56 +0200

    do update on live - tests ok

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #mkdir -p ../../../../../db_backup
-#drush sql-dump > ../../../../../db_backup/$(date +"%Y-%m-%d__%H-%M-%S".sql)
+drush sql-dump > ../../../../../db_backup/$(date +"%Y-%m-%d__%H-%M-%S".sql)
 #drush sql-drop -y
 #drush sql-cli < ../../../../../db_backup/2017-04-28__16-15-01_from_live.sql
 
@@ -18,6 +18,6 @@ drush dis -y overlay
 drush en -y map_view_update
 drush fr -y map_view_update
 
-drush user-password Romalu3 --password="test"
+#drush user-password Romalu3 --password="test"
 
 drush cc all
