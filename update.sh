0a46ef9b840bc27c1a078c02b0c9c8a07cc1b00a
hissDev <rafael.hiss@gmail.com>
2017-05-08 17:22:16 +0200

    Profile update

--- update.sh
+++ update.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/env bash
 
-git pull origin dev
+git pull origin master
+cd ../../themes/future_history/ && git pull origin master && cd -
+cd ../futurehistory/ && git pull origin master && cd -
+drush cc all
 
-#mkdir -p ../../../../../db_backup
-#mkdir -p ~/db_backup
-#cp sites/default/files ~/db_backup/sites/default/files/
-#mkdir -p ~/db_backup/sites/default/files/
-#drush sql-dump > ~/db_backup/$(date +"%Y-%m-%d__%H-%M-%S".sql)
-#drush sql-dump > ../../../../../db_backup/$(date +"%Y-%m-%d__%H-%M-%S".sql)
-#drush sql-dump > ../db_backup/$(date +"%Y-%m-%d__%H-%M-%S".sql)
-#drush sql-drop -y
-#drush sql-cli < 2017-02-01__15-05-29.sql
-
-
-cd ../../themes/future_history/ && git pull origin dev && cd -
+drush en -y map_view_update
+drush fr -y map_view_update
 
 drush cc all
