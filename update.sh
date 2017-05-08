79f03bd6d240ba9269da7bfc64468a1e992dc652
hissDev <rafael.hiss@gmail.com>
2017-05-08 17:34:57 +0200

    wip

--- update.sh
+++ update.sh
@@ -5,6 +5,9 @@ cd ../../themes/future_history/ && git pull origin master && cd -
 cd ../futurehistory/ && git pull origin master && cd -
 drush cc all
 
+
+drush cc all
+drush dis -y overlay
 drush en -y map_view_update
 drush fr -y map_view_update
 
